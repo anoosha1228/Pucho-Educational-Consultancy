c90f908b00df91572c742335211349a04197aa75
anoosha1228 <89073322+anoosha1228@users.noreply.github.com>
2022-08-07 01:52:44 +0500

    Update settings.py

--- fyp_pucho/settings.py
+++ fyp_pucho/settings.py
@@ -136,5 +136,5 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'atanseer2016@gmail.com'
-EMAIL_HOST_PASSWORD = 'odlculhmthmqytxl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
